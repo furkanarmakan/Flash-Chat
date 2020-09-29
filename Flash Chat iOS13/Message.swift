c928b7168ac2f5d8d7bcd5c65a6a37407d948e31
Furkan Armakan <39617002+furkanarmakan@users.noreply.github.com>
2020-09-29 17:15:41 +0300

    Add files via upload

---
+++ Flash Chat iOS13/Message.swift
@@ -0,0 +1,14 @@
+//
+//  Message.swift
+//  Flash Chat iOS13
+//
+//  Created by Furkan Armakan on 30.05.2020.
+//  Copyright © 2020 Angela Yu. All rights reserved.
+//
+
+import Foundation
+
+struct Message {
+    let sender: String // email adress of the person who send this messages
+    let body: String // Body of a messages
+}
